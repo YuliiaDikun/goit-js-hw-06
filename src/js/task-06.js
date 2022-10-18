f333d40cd420282858cc831843c2ff93ac37ffdb
unknown <dikun.yulia@gmail.com>
2022-10-18 10:37:33 +0300

    task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('blur', () => {    
+    if (inputEl.value.length != inputEl.dataset.length) {
+        inputEl.classList.add('invalid');
+    } else {
+        inputEl.classList.add('valid');
+    }
+});
