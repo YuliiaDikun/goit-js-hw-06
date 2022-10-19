9053b4872b86bb8d16c9aa5393975c11f3fdbab2
unknown <dikun.yulia@gmail.com>
2022-10-19 20:11:41 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', formValidation);
+
+function formValidation(e) {  
+   e.preventDefault();
+  const {
+    elements: { email, password }
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  e.currentTarget.reset();
+}
