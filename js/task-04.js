8a2dffad41ecaf57dc46c50027e95da0a14986bc
unknown <dikun.yulia@gmail.com>
2022-10-20 17:08:33 +0300

    task 4 fixed

--- js/task-04.js
+++ js/task-04.js
@@ -2,10 +2,14 @@ const valueEl = document.querySelector('#value');
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 
+let counterValue = 0;
+
 decrementBtn.addEventListener('click', () => {
-    return valueEl.textContent = +valueEl.textContent - 1;
+    counterValue--;   
+    return valueEl.textContent = counterValue;
 });
 
 incrementBtn.addEventListener('click', () => {
-    return valueEl.textContent = +valueEl.textContent + 1;
+    counterValue++;   
+    return valueEl.textContent = counterValue;
 });
