1ec2cd61e1ed2be43805ff7c695148720bb86f99
unknown <dikun.yulia@gmail.com>
2022-10-20 17:09:47 +0300

    task 5 fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,5 @@ const inputEl = document.querySelector('#name-input');
 const spanEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', () => {
-    return inputEl.value !== '' ? spanEl.textContent = inputEl.value : spanEl.textContent = 'Anonymous';
+    return inputEl.value ? spanEl.textContent = inputEl.value : spanEl.textContent = 'Anonymous';
 });
